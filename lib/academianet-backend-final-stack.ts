e9cdd39351b93b4fe931d23d55e00e55f3301829
JuanCarrasquilla360 <juancarrasquilla135219@hotmail.com>
2025-03-13 21:25:39 -0500

    first commit

--- lib/academianet-backend-final-stack.ts
+++ lib/academianet-backend-final-stack.ts
@@ -1,11 +1,12 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { RestApiConstruct } from "devarchy-cdk";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AcademianetBackendFinalStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
+    const api = new RestApiConstruct(this, "apiTest");
     // The code that defines your stack goes here
 
     // example resource
